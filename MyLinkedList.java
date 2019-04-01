12fee52e120f50aa7106b634589723458f3c35f7
AmitPr <17amitprasad@gmail.com>
2019-04-01 12:10:26 -0400

    Finished sort & fixed some null pointers

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -4,52 +4,105 @@ public class MyLinkedList<E> {
     private Node last;
 
     public MyLinkedList() {
-        first=last=null;
+        first = last = null;
     }
+
     public int getSize() {
         return size;
     }
-    public void clear(){
-        first=last=null;
+
+    public void clear() {
+        first = last = null;
+        size = 0;
     }
-    public void addFirst(E data){
-        Node n = new Node(data, first,null);
-        first.setPrevious(n);
-        first=n;
+
+    public void addFirst(E data) {
+        Node n = new Node(data, first, null);
+        if (first == null) {
+            first = last = n;
+        } else {
+            first.setPrevious(n);
+            first = n;
+        }
+        size++;
     }
-    public void addLast(E data){
-        Node n = new Node(data,null,last);
-        last.setNext(n);
-        last=n;
+
+    public void addLast(E data) {
+        Node n = new Node(data, null, last);
+        if (last == null) {
+            first = last = n;
+        } else {
+            last.setNext(n);
+            last = n;
+        }
+        size++;
     }
-    public void extend(MyLinkedList<E> other){
+
+    public void extend(MyLinkedList<E> other) {
         Node n = other.getFirstNode();
-        last.setNext(n);
-        n.setPrevious(last);
+        if (n != null) {
+            if (last == null) {
+                first = n;
+                last = other.getLastNode();
+            } else {
+                last.setNext(n);
+                n.setPrevious(last);
+                last = other.getLastNode();
+            }
+            size += other.getSize();
+        }
         other.clear();
     }
-    public Node getFirstNode(){
+
+    public E removeFirst() {
+        E data = first.data;
+        if (first.next != null) {
+            first.next.setPrevious(null);
+            first = first.next;
+        } else {
+            first = null;
+            last=null;
+        }
+        size--;
+        return data;
+    }
+
+    public Node getFirstNode() {
         return first;
     }
-    public Node getLastNode(){
+
+    public Node getLastNode() {
         return last;
     }
-    public String toString(){
+    public E getFirst(){
+        if(first!=null){
+            return first.data;
+        }
+        return null;
+    }
+    public E getLast(){
+        if(last!=null){
+            return last.data;
+        }
+        return null;
+    }
+
+    public String toString() {
         StringBuilder sb = new StringBuilder("[");
         Node cur = first;
-        if(cur!=null){
-            sb.append(cur.getData().toString());
-        }else{
+        if (cur != null) {
+            sb.append(cur.getData().toString() + ", ");
+            while (cur.getNext() != null) {
+                sb.append(cur.getNext().getData().toString() + ", ");
+                cur = cur.getNext();
+            }
+        } else {
             sb.append(", ");
         }
-        while(cur.getNext()!=null){
-            sb.append(cur.getNext().toString()+", ");
-            cur=cur.getNext();
-        }
-        sb.replace(sb.length()-2, sb.length(), "]");
-        sb.append("]");
+        sb.replace(sb.length() - 2, sb.length(), "]");
         return sb.toString();
     }
+
     private class Node {
         private E data;
         private Node next;
@@ -66,23 +119,29 @@ public class MyLinkedList<E> {
             this.next = next;
             this.previous = previous;
         }
-        public E getData(){
+
+        public E getData() {
             return data;
         }
-        public void setData(E data){
-            this.data=data;
+
+        public void setData(E data) {
+            this.data = data;
         }
-        public Node getNext(){
+
+        public Node getNext() {
             return next;
         }
-        public void setNext(Node next){
-            this.next=next;
+
+        public void setNext(Node next) {
+            this.next = next;
         }
-        public Node getPrevious(){
+
+        public Node getPrevious() {
             return previous;
         }
-        public void setPrevious(Node previous){
-            this.previous=previous;
+
+        public void setPrevious(Node previous) {
+            this.previous = previous;
         }
     }
 }
--- Radix.java
+++ Radix.java
@@ -1,5 +1,80 @@
-public class Radix{
+import java.util.Arrays;
+public class Radix {
+    public static void main(String[]args){
+        System.out.println("Size\t\tMax Value\tquick/builtin ratio ");
+        int[]MAX_LIST = {1000000000,500,10};
+        for(int MAX : MAX_LIST){
+          for(int size = 31250; size < 2000001; size*=2){
+            long qtime=0;
+            long btime=0;
+            //average of 5 sorts.
+            for(int trial = 0 ; trial <=5; trial++){
+              int []data1 = new int[size];
+              int []data2 = new int[size];
+              for(int i = 0; i < data1.length; i++){
+                data1[i] = (int)(Math.random()*MAX);
+                data2[i] = data1[i];
+              }
+              long t1,t2;
+              t1 = System.currentTimeMillis();
+              Radix.radixsort(data2);
+              t2 = System.currentTimeMillis();
+              qtime += t2 - t1;
+              t1 = System.currentTimeMillis();
+              Arrays.sort(data1);
+              t2 = System.currentTimeMillis();
+              btime+= t2 - t1;
+              if(!Arrays.equals(data1,data2)){
+                System.out.println("FAIL TO SORT!");
+                System.exit(0);
+              }
+            }
+            System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
+          }
+          System.out.println();
+        }
+      }
     public static void radixsort(int[] data){
-        
+        @SuppressWarnings("unchecked")
+        MyLinkedList<Integer>[] buckets= (MyLinkedList<Integer>[]) new MyLinkedList[10];
+        for(int i = 0; i < buckets.length; i++){
+            buckets[i]=new MyLinkedList<Integer>();
+        }
+        int largest = Integer.MIN_VALUE;
+        int curDigit = 1;
+        for(int i = 0; i < data.length; i++){
+            if(data[i]>largest) largest = data[i];
+            if(data[i]<0)
+                buckets[digitAt(data[i], curDigit)].addFirst(data[i]);
+            else
+                buckets[digitAt(data[i],curDigit)].addLast(data[i]);
+        }
+        int maxDigit = (int)Math.ceil(Math.log10(largest));
+        MyLinkedList<Integer> curWork = new MyLinkedList<Integer>();
+        merge(curWork,buckets);
+        curDigit++;
+        while(curDigit <= maxDigit){
+            for(int i = 0; i < data.length; i++){
+                int x = curWork.removeFirst();
+                if(x<0)
+                    buckets[digitAt(x, curDigit)].addFirst(x);
+                else
+                    buckets[digitAt(x,curDigit)].addLast(x);
+            }
+            merge(curWork,buckets);
+            curDigit++;
+        }
+        for(int i = 0; i < data.length; i++){
+            data[i]=curWork.removeFirst();
+        }
+    }
+    private static void merge(MyLinkedList<Integer> into, MyLinkedList<Integer>[] buckets){
+        for(MyLinkedList<Integer> bucket : buckets){
+            into.extend(bucket);
+        }
+    }
+    private static int digitAt(int n, int digit) {
+        int d = (n / ((int) Math.pow(10, digit - 1))) % 10;
+        return d;
     }
 }
